48c564e738f11e1ca9ed0f6e2507e3895cdb2454
Koda Bryant <31840899+PrinceVenn@users.noreply.github.com>
2017-09-18 00:40:46 -0700

    Create puzzle-9.js

---
+++ puzzle-9.js
@@ -0,0 +1,67 @@
+function repeatDown(total)
+{
+	var count = 0;
+  while (count < total) {
+    down();
+    count = count + 1;
+  }
+}
+
+
+
+down();
+var a = getColor();
+getColor();
+down();
+var b = getColor();
+getColor();
+down();
+var c = getColor();
+getColor();
+down();
+var d = getColor();
+getColor();
+down();
+var e = getColor();
+getColor();
+down();
+var f = getColor();
+getColor();
+down();
+var g = getColor();
+getColor();
+down();
+var h = getColor();
+getColor();
+down();
+right();
+setColor(a);
+right();
+up();
+setColor(b);
+right();
+up();
+setColor(c);
+right();
+up();
+setColor(d);
+right();
+up();
+setColor(e);
+right();
+up();
+setColor(f);
+right();
+up();
+setColor(g);
+right();
+up();
+setColor(h)
+right();
+up();
+up();
+
+
+
+
+
