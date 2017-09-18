bf2c2b4b1bdc6939e1f19898f584c41771ce3c4d
Koda Bryant <31840899+PrinceVenn@users.noreply.github.com>
2017-09-18 00:37:44 -0700

    Create puzzle-5.js

---
+++ puzzle-5.js
@@ -0,0 +1,19 @@
+function upDown()
+{
+if(getColor()=="blue") {
+down();
+right();
+right();
+} else {
+if(getColor() == "red"){
+up();
+right();
+right();}
+}
+}
+right();
+right();
+upDown();
+upDown();
+upDown();
+right();
