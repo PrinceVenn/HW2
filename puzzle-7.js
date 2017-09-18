d823259a06e9b6af67596d78596a26e9939180d8
Koda Bryant <31840899+PrinceVenn@users.noreply.github.com>
2017-09-18 00:39:15 -0700

    Create puzzle-7.js

---
+++ puzzle-7.js
@@ -0,0 +1,42 @@
+function downM() 
+{
+while (getColor() != "red")
+down();
+}       
+function upM()
+{
+while (getColor() != "red")
+up();
+}
+    
+    
+downM();
+if (getColor()== "red")
+{
+right();
+right();
+}
+upM();
+if (getColor()== "red")
+{
+right();
+right();
+}
+downM();
+if (getColor()== "red")
+{
+right();
+right();
+}
+upM();
+  if (getColor()== "red")
+{
+right();
+right();
+}
+downM();
+if (getColor()== "red")
+{
+right();
+right();
+}
