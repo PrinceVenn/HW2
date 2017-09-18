a9ce8407824270eea12bc0f74d24eb6b9a964d73
Koda Bryant <31840899+PrinceVenn@users.noreply.github.com>
2017-09-18 00:36:45 -0700

    Create puzzle-4.js

---
+++ puzzle-4.js
@@ -0,0 +1,18 @@
+function boxColor(){
+up()
+var firstColor = getColor();
+down();
+setColor(firstColor);
+right();
+right();}
+
+
+down();
+down();
+down();
+down();
+down();
+right();
+boxColor();
+right();
+boxColor();
